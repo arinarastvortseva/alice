5740a30e1e9d79ca1f71c8b80c6dff5bc14aaa72
arinarastvortseva <71328510+arinarastvortseva@users.noreply.github.com>
2021-05-02 19:22:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ def resp():
 
     end = False
 
-    if text == 'Выход':
+    if text == 'выход':
         response_text = 'Всего хорошего! Приходите позже, мы вас ждём!'
         end = True
-
     elif text:
-        response_text = 'Жили были трали вали тут должен быть сценарий '
-
+        response_text = 'Жили были трали вали тут должен быть сценарий'
     else:
         response_text = 'Добро пожаловать в игровой навык, который еще не разработан! Надеюсь еще не дедлайн!!!!!'
 
     response = {
         'response': {
             'text': response_text,
+            "card": {
+                "type": "BigImage",
+                "image_id": "965417/f8b5c546452c24dd1e44",
+                "title": "Супер пупер мега квест",
+                "description": "тут должно быть краткое описание что вообще происходит мрпсрачрачпяпвя",
+
+            },
             'end_session': end,
             'buttons': [
-                {'title':'Я хочу начать игру!', 'hide': True},
-                {'title':'Выход', 'hide': True}
+                {'title': 'Я хочу начать игру!', 'hide': True},
+                {'title': 'выход', 'hide': True},
             ]
         },
         'version': '1.0'
