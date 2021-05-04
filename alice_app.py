cf250394e583719efec8cdd9b0d01026223d8ac4
arinarastvortseva <71328510+arinarastvortseva@users.noreply.github.com>
2021-05-05 01:52:45 +0300

    Add files via upload

---
+++ alice_app.py
@@ -0,0 +1,41 @@
+# Импортируем модули для работы с логами.
+import logging
+
+# Импортируем модуль для работы с API Алисы
+from alice_sdk import AliceRequest, AliceResponse
+
+# Импортируем модуль с логикой игры
+from main import handle_dialog
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+session_storage = {}
+
+# Задаем параметры приложения Flask.
+@app.route('/alice', methods=['POST'])
+
+def main():
+
+    # Функция получает тело запроса и возвращает ответ.
+    alice_request = AliceRequest(request.json)
+    logging.info('Request: {}'.format(alice_request))
+
+    alice_response = AliceResponse(alice_request)
+
+    user_id = alice_request.user_id
+
+    alice_response, session_storage[user_id] = handle_dialog(
+        alice_request, alice_response, session_storage.get(user_id)
+    )
+
+    logging.info('Response: {}'.format(alice_response))
+
+    return alice_response.dumps()
+
+
+app.run('0.0.0.0', port=5000, debug=True)
