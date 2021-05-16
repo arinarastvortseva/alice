383c1ba6f5c1252cf9ba580a383ed4313863fdc0
arinarastvortseva <71328510+arinarastvortseva@users.noreply.github.com>
2021-05-16 20:56:25 +0300

    Add files via upload
    final

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 from itertools import cycle
 
-
 with open('script-for-alice.csv', 'r', encoding='utf8') as csvfile:
     data = csv.DictReader(csvfile, delimiter=';', quotechar=' ')
     events = {x['event']: [x['action'], x['branch'], x['image']] for x in data}
@@ -9,13 +8,16 @@ with open('script-for-alice.csv', 'r', encoding='utf8') as csvfile:
 
 # Функция для непосредственной обработки диалога.
 def handle_dialog(request, response, user_storage):
-
     if request.is_new_session:
         user_storage = {}
-        response.set_text('объяснялочка что здесь происходит и начало, чтобы уже можно было выбрат разветвление!'
+        response.set_text('Для завершения игры скажите "конец игры".\n')
+        response.set_buttons([{'title': 'подождать еще день', 'hide': True},
+                              {'title': 'выбираться самой', 'hide': True}])
+        response.set_image('1030494/2e21639ad34cd97d8c9c', 'Милой Элитии исполнилось уже 20 лет, она стояла '
+                          'у окна и рассматривала плывущие по небу облака.'
+                          'Красавица думала про себя: '
+                          '“Подождать мне еще один день или стоит уже выбираться самой?'
                           'Для завершения игры скажите "конец игры".\n')
-        response.set_buttons([{'title': 'branch 1', 'hide': True},
-                             {'title': 'branch 2', 'hide': True}])
         return response, user_storage
 
     else:
@@ -27,7 +29,7 @@ def handle_dialog(request, response, user_storage):
 
             return response, user_storage
 
-        elif request.command.lower() in ['branch 1']:
+        elif request.command in ['подождать еще день']:
             a = []
             b = []
 
@@ -37,11 +39,10 @@ def handle_dialog(request, response, user_storage):
 
             inf_list = cycle(a)
             image_list = cycle(b)
-            print(b)
             user_storage['questions'] = inf_list
             user_storage['pictures'] = image_list
 
-            event = next(user_storage['questions']
+            event = next(user_storage['questions'])
             action = events[event][0]
             image = events[event][2]
             buttons = get_buttons(action)
@@ -49,18 +50,18 @@ def handle_dialog(request, response, user_storage):
             user_storage['event'] = event
             user_storage['action'] = action
             user_storage['buttons'] = buttons
-            response.set_text(format(user_storage['event']))
+            response.set_text(user_storage['event'])
             response.set_buttons(user_storage['buttons'])
             response.set_image(image, event)
             return response, user_storage
 
-        elif request.command.lower() in ['branch 2']:
+        elif request.command in ['выбираться самой']:
             a = []
             b = []
             print(events.keys())
             for x in events.keys():
                 print(events[x][1])
-                if events[x][1] == 'branch 2':
+                if events[x][1] == 'выбираться самой':
                     a.append(x)
                     b.append(events[x][2])
             print(a)
@@ -72,7 +73,7 @@ def handle_dialog(request, response, user_storage):
             user_storage['pictures'] = image_list
 
             event = next(user_storage['questions'])
-            
+
             action = events[event][0]
             image = events[event][2]
             buttons = get_buttons(action)
@@ -80,12 +81,12 @@ def handle_dialog(request, response, user_storage):
             user_storage['event'] = event
             user_storage['action'] = action
             user_storage['buttons'] = buttons
-            response.set_text(format(user_storage['event']))
+            response.set_text(user_storage['event'])
             response.set_buttons(user_storage['buttons'])
             response.set_image(image, event)
             return response, user_storage
 
-        elif request.command.lower() == user_storage['action']:
+        elif request.command == user_storage['action']:
             # Пользователь выбрал что-то.
             event = next(user_storage['questions'])
             action = events[event][0]
@@ -94,12 +95,12 @@ def handle_dialog(request, response, user_storage):
             user_storage['event'] = event
             user_storage['action'] = action
             user_storage['buttons'] = buttons
-            response.set_text(format(user_storage['event']))
+            response.set_text(user_storage['event'])
             response.set_buttons(user_storage['buttons'])
             response.set_image(image, event)
             return response, user_storage
-       
-        response.set_text(format(user_storage['event']))
+
+        response.set_text(user_storage['event'])
         return response, user_storage
 
 
