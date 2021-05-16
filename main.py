3752878bc01691375717b72a884ce06907b4f2b8
arinarastvortseva <71328510+arinarastvortseva@users.noreply.github.com>
2021-05-16 17:37:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import csv
 from itertools import cycle
-
+from images import Image
+from alice_sdk import AliceResponse
 
 with open("script-for-alice.csv", "r", encoding="utf8") as csvfile:
-    data = csv.DictReader(csvfile, delimiter=",", quotechar=" ")
-    events = {x['event']: [x['action'], x['branch']] for x in data}
-
+    data = csv.DictReader(csvfile, delimiter=";", quotechar=" ")
+    events = {x['event']: [x['action'], x['branch'], x['image']] for x in data}
 
 # Функция для непосредственной обработки диалога.
 def handle_dialog(request, response, user_storage):
+
+
     if request.is_new_session:
         user_storage = {}
         response.set_text('объяснялочка что здесь происходит и начало, чтобы уже можно было выбрат разветвление!'
@@ -26,25 +28,70 @@ def handle_dialog(request, response, user_storage):
 
             return response, user_storage
 
-        elif request.command.lower() in ['branch 1', 'branch 2']:
+        elif request.command.lower() in ['branch 1']:
             a = []
             b = []
+
             for x in events.keys():
                 a.append(x)
-                b.append(events[x][0])
+                b.append(events[x][2])
+
             inf_list = cycle(a)
+            image_list = cycle(b)
+            print(b)
             user_storage['questions'] = inf_list
+            user_storage['pictures'] = image_list
+
 
             event = next(user_storage['questions'])
+            #image = next(user_storage['pictures'])
             action = events[event][0]
+            image = events[event][2]
             buttons = get_buttons(action)
+
 
             user_storage["event"] = event
             user_storage["action"] = action
             user_storage["buttons"] = buttons
             response.set_text(format(user_storage["event"]))
             response.set_buttons(user_storage["buttons"])
-            # response.set_image(image)
+            response.set_image(image, event)
+
+
+            return response, user_storage
+
+        elif request.command.lower() in ['branch 2']:
+            a = []
+            b = []
+            print(events.keys())
+            for x in events.keys():
+                print(events[x][1])
+                if events[x][1] == 'branch 2':
+                    a.append(x)
+                    b.append(events[x][2])
+            print(a)
+            print(b)
+            inf_list = cycle(a)
+            image_list = cycle(b)
+            print(b)
+            user_storage['questions'] = inf_list
+            user_storage['pictures'] = image_list
+
+
+            event = next(user_storage['questions'])
+            #image = next(user_storage['pictures'])
+            action = events[event][0]
+            image = events[event][2]
+            buttons = get_buttons(action)
+
+
+            user_storage["event"] = event
+            user_storage["action"] = action
+            user_storage["buttons"] = buttons
+            response.set_text(format(user_storage["event"]))
+            response.set_buttons(user_storage["buttons"])
+            response.set_image(image, event)
+
 
             return response, user_storage
 
@@ -52,20 +99,18 @@ def handle_dialog(request, response, user_storage):
             # Пользователь выбрал что-то.
             event = next(user_storage['questions'])
             action = events[event][0]
+            image = events[event][2]
             buttons = get_buttons(action)
             user_storage["event"] = event
             user_storage["action"] = action
             user_storage["buttons"] = buttons
             response.set_text(format(user_storage["event"]))
             response.set_buttons(user_storage["buttons"])
-
+            response.set_image(image, event)
             return response, user_storage
-
-        buttons = get_buttons(user_storage['action'])
-
-        response.set_buttons(buttons)
+        # buttons = get_buttons(user_storage['action'])
+        # response.set_buttons(buttons)
         response.set_text(format(user_storage["event"]))
-
         return response, user_storage
 
 
@@ -75,3 +120,5 @@ def get_buttons(action):
 
     buttons = [{'title': actions, 'hide': True}]
     return buttons
+
+
